910c1dc4bbc5dbeb4ece25d139fdd655ba1c2404
Семён Стульников <sema.stulnikov@mail.ru>
2021-11-20 22:09:23 +0300

    brain-even

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 import helloUser from '../src/cli.js';
+
 helloUser();
--- src/games/g-brain-even.js
+++ src/games/g-brain-even.js
@@ -5,35 +5,28 @@ const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 const BrainEven = () => {
-    for (let i = 1; i < 4; i += 1) {
-        const randomNum = Math.floor(Math.random() * 1000);
-        console.log('Question:', randomNum);
-        const player = readlineSync.question('Your answer: ');
-        if (randomNum % 2 === 0) {
-            if (player === 'yes') {
-                console.log('Correct!')
-            }
-
-            else {
-                console.log(`'${player}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${userName}!`); 
-                return;
-            }
-           
-        }
-        if (randomNum % 2 != 0) {
-            if (player === 'no') {
-                console.log('Correct!')
-            }
-
-            else {
-                console.log(`'${player}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${userName}!`); 
-                return;
-            }
-           
-        }
-
+  for (let i = 1; i < 4; i += 1) {
+    const randomNum = Math.floor(Math.random() * 1000);
+    console.log('Question:', randomNum);
+    const player = readlineSync.question('Your answer: ');
+    if (randomNum % 2 === 0) {
+      if (player === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${player}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, ${userName}!`);
+        return;
+      }
     }
-    console.log(`Congratulations, ${userName}!`)   
+    if (randomNum % 2 !== 0) {
+      if (player === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${player}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, ${userName}!`);
+        return;
+      }
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default BrainEven;
