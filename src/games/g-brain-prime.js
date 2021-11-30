aefddf047ec59d96fc08d3fd00dfe735b5da6788
Семён Стульников <sema.stulnikov@mail.ru>
2021-11-30 23:41:51 +0300

    fix3

--- src/games/g-brain-prime.js
+++ src/games/g-brain-prime.js
@@ -2,6 +2,9 @@ import randomNum from '../random_num.js';
 
 const intager = (randomNumber) => {
   let resault = true;
+  if (randomNumber === 1) {
+    return false;
+  }
   for (let i = 2; i < randomNumber; i += 1) {
     if (randomNumber % i === 0) {
       resault = false;
@@ -12,7 +15,7 @@ const intager = (randomNumber) => {
 };
 
 const BrainPrime = () => {
-  const randomNumber = randomNum(1, 100);
+  const randomNumber = randomNum(1, 2);
   const question = randomNumber;
   const correctAnswer = intager(randomNumber) ? 'yes' : 'no';
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
