2a7d2e56baeb22f19d108019898bf123cb0054c2
Семён Стульников <sema.stulnikov@mail.ru>
2021-12-08 21:06:46 +0300

    fix5

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const numberRounds = 3;
 const EngineGame = (Game) => {
-  console.log('Welcom to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   const [round, rules] = Game();
