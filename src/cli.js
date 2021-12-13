aac746c6e87cb2baa0b88979ff0cf9265a153587
Семён Стульников <sema.stulnikov@mail.ru>
2021-12-13 23:21:24 +0300

    fix7.1

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const helloUser = () => {
-  console.log('Welcom to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
